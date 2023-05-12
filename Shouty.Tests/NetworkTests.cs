0c1068a7a7ff641e90390defcae38c22e640bce7
Ismail Karabulut <i.karabulut93@gmail.com>
2023-05-12 14:21:39 -0500

    add initial unit tests

---
+++ Shouty.Tests/NetworkTests.cs
@@ -0,0 +1,14 @@
+namespace Shouty.Tests;
+
+public class NetworkTests
+{
+    [Fact]
+    public void Broadcasts_a_message_to_all_listeners()
+    {
+        var network = new Network();
+        const string message = "Free bagels!";
+        var lucy = new Person(network);
+        network.Broadcast(message);
+        Assert.Contains(message, lucy.GetMessagesHeard());
+    }
+}
