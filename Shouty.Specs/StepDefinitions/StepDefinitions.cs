cfd0f221d61d8860d3f5972af81164edbe845145
Ismail Karabulut <i.karabulut93@gmail.com>
2023-05-12 14:43:38 -0500

    remove duplicate code

--- Shouty.Specs/StepDefinitions/StepDefinitions.cs
+++ Shouty.Specs/StepDefinitions/StepDefinitions.cs
@@ -10,30 +10,32 @@ public partial class StepDefinitions
     private Person _lucy;
     private Person _sean;
     private string _messageFromSean;
-    private Network _network = new Network();
+    private Network _network;
+    private Dictionary<string, Person> _people;
+
+    [BeforeScenario]
+    public void CreateNetwork()
+    {
+        _network = new Network();
+        _people = new Dictionary<string, Person>();
+    }
 
     [When("Sean shouts {string}")]
     public void WhenSeanShouts(string message)
     {
-        _sean.Shout(message);
+        _people["Sean"].Shout(message);
         _messageFromSean = message;
     }
 
     [Then("Lucy hears Sean's message")]
     public void ThenPersonHearsSeansMessage()
     {
-        Assert.Contains(_messageFromSean, _lucy.GetMessagesHeard());
+        Assert.Contains(_messageFromSean, _people["Lucy"].GetMessagesHeard());
     }
 
-    [Given(@"a person named Lucy")]
-    public void GivenAPersonNamedLucy()
+    [Given("a person named {word}")]
+    public void GivenAPersonNamedLucy(string name)
     {
-        _lucy = new Person(_network);
-    }
-
-    [Given(@"a person named Sean")]
-    public void GivenAPersonNamedSean()
-    {
-        _sean = new Person(_network);
+        _people.Add(name, new Person(_network));
     }
 }
